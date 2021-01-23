012796e2a95a5465f758369f703cd0380599806a
HangorG <gaba93@yandex.ru>
2021-01-23 20:31:58 +0300

    insert X,O into the DOM on click

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Game Module
 const gameModule = (function () {
   const gameContainer = document.querySelector(".main-container");
   const gameBoard = [...document.querySelectorAll(".cell")];
@@ -16,17 +17,24 @@ const gameModule = (function () {
   return { currentPlayer };
 })();
 
+// Display module
 const displayModule = (function () {
-
   function render(e) {
-    if (!e.target.textContent) {
-      createImg
+    if (!e.target.hasChildNodes()) {
+      e.target.appendChild(createImgEl());
     }
+  }
+
+  function createImgEl() {
+    const img = document.createElement("img");
+    img.src = gameModule.currentPlayer().symbol;
+    return img;
   }
 
   return { render };
 })();
 
+// Player Factory function
 const playerFactory = function (name) {
   const symbol = name === "Player1" ? "cancel.svg" : "circle.svg";
   return { name, symbol };
