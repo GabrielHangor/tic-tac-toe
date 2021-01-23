8ed8c6db5f12129b966d99f7206b0a257fb651f3
HangorG <gaba93@yandex.ru>
2021-01-23 22:23:00 +0300

    fixed renderEl bug

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
 // Game Module
 const gameModule = (function () {
   const gameContainer = document.querySelector(".main-container");
-  const gameBoard = [...document.querySelectorAll(".cell")];
+  const gameBoard = ["", "", "", "", "", "", "", "", ""];
   let counter = 0;
+
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   function currentPlayer() {
     const playerName = counter % 2 === 0 ? "Player1" : "Player2";
     const player = playerFactory(playerName);
     counter++;
+    console.log(counter);
 
     return player;
   }
 
-  gameContainer.addEventListener("click", (e) => displayModule.render(e));
+  // function updateGameBoardArray(e) {
+  //   const index = e.target.getAttribute("data-num");
+
+  //     // const el = e.target.firstChild.src.includes("cancel.svg")
+  //     //   ? "Cross"
+  //     //   : "Circle";
+
+  // }
+
+  gameContainer.addEventListener("click", (e) => displayModule.renderEl(e));
 
   return { currentPlayer };
 })();
 
 // Display module
 const displayModule = (function () {
-  function render(e) {
-    if (!e.target.hasChildNodes()) {
-      e.target.appendChild(createImgEl());
+  function renderEl(e) {
+    if (!e.target.hasChildNodes() && e.target.className !== "img") {
+      e.target.appendChild(createImgEl(e));
     }
   }
 
-  function createImgEl() {
+  function createImgEl(e) {
     const img = document.createElement("img");
     img.src = gameModule.currentPlayer().symbol;
+    img.className = "img";
     return img;
   }
 
-  return { render };
+  return { renderEl };
 })();
 
 // Player Factory function
